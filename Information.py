c1a6c2ffbf55b7f4003a33efd0421b5d822ceba2
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2023-07-13 18:29:25 +0800

    added welcome label and start button for 1st panel

--- Information.py
+++ Information.py
@@ -1,4 +1,19 @@
 # pseudocode
+from tkinter import *
+
+start = Tk()
+start.title("COVID Contact Tracing")
+start.geometry("500x500")
+
+# titles
+
+welcome = Label(start, text = "Welcome")
+welcome.pack()
+
+#buttons
+submit = Button(start, text = "Start") 
+submit.pack()
+start.mainloop()
 # get information (name, age, bday, address, contact no., etc.)
 # questions about covid status (vaccinated or booster?, symptoms, exposure to probable cause, in contact with someone, tested for covid?)
 # submits the data and save in a file
