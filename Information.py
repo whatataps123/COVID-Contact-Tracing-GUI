77038adbb36dd19a46cf5bdc1908eb21ab869559
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2023-07-13 17:59:35 +0800

    pseudocode

---
+++ Information.py
@@ -0,0 +1,7 @@
+# pseudocode
+# get information (name, age, bday, address, contact no., etc.)
+# questions about covid status (vaccinated or booster?, symptoms, exposure to probable cause, in contact with someone, tested for covid?)
+# submits the data and save in a file
+# Add or search entry
+# loop
+# End
