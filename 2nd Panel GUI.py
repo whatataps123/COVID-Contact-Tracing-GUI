9d2374c36f74f0cb4e65bd69e2b4d10a0d59a39d
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2023-07-15 13:49:11 +0800

    added more contact details for 2nd GUI

--- 2nd Panel GUI.py
+++ 2nd Panel GUI.py
@@ -9,7 +9,7 @@ panel_2.title("Personal Information")
 frame = Frame(panel_2)
 frame.pack()
 
-# Enter Data for User
+### PERSONAL INFORMATION
 user_frame = LabelFrame(frame, text="Personal Information")
 user_frame.grid(row=0, column=0, padx= 20, pady = 20)
 
@@ -30,5 +30,30 @@ sex_label = Label(user_frame, text="Biological Sex")
 sex_label.grid(row=0, column=2)
 sex_box = ttk.Combobox(user_frame, values=["Male","Female"])
 sex_box.grid(row=1, column= 2)
+
+# Age
+age_label = Label(user_frame, text="Age")
+age_label.grid(row=0, column=3)
+age_box = ttk.Spinbox(user_frame, from_=1, to_=120, width=3)
+age_box.grid(row=1,column=3)
+
+# Enter Address
+address_label = Label(user_frame,text="Address")
+address_label.grid(row=2,columnspan=4)
+address_entry = Entry(user_frame, width=71)
+address_entry.grid(row=3, columnspan=10)
+
+# Enter Number
+contact_num_label = Label(user_frame, text="Contact Number")
+contact_num_label.grid(row=4,column=0)
+contact_num_entry = Entry(user_frame)
+contact_num_entry.grid(row=5, column=0)
+
+# Enter Email
+email_label = Label(user_frame, text="Email")
+email_label.grid(row=4,column=1)
+email_label_entry = Entry(user_frame)
+email_label_entry.grid(row=5,column=1)
+
 
 panel_2.mainloop()
